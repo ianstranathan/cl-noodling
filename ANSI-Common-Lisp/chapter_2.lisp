275335c968b3fd86a1d26500834bcad6ec210324
Stranathan <nimsec@ian.lan>
2023-04-11 23:39:49 -0400

    chpt 2 ANSI CL

--- ANSI-Common-Lisp/chapter_2.lisp
+++ ANSI-Common-Lisp/chapter_2.lisp
@@ -30,3 +30,39 @@
 	  (setf ret element)))
     ret))
 	  
+;; # 8
+;; Give iterative and recursion definitions of a func that takes in an integer and prints out as many dots
+
+(defun print-out-n-dots-iterative(some-integer)
+  (do ((i 0 (+ i 1)))
+      ((= i some-integer))
+    (format t ".~%")))  
+    
+(defun print-out-n-dots-iterative(some-integer)
+  (dotimes (i some-integer)
+    (format t ". ")))
+
+(defun print-out-n-dots-recursive(some-integer)
+  (if (equal some-integer 0)
+      (format t "~%")
+      (progn (format t ". ") (print-out-n-dots-recursive (- some-integer 1)))))
+
+;; takes a list and returns the number of times the symbol a occurs in it
+(defun num-repeating-a-iterative( ls )
+  (let ( (s 0))
+    (dolist (i ls)
+      (if (equal i 'a)
+	  (setf s (+ 1 s))))
+    s))
+
+(defun num-repeating-a-recursive (ls) 
+  (if ls
+      (+ (if (equal (car ls) 'a)
+	     1
+	     0)
+	 (a-rec (cdr ls)))
+      0))
+  
+
+      
+  
